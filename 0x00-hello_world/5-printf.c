9d2ea334791aa816d9bbeeddcc38fd1580de6ae2
erminegn <ermiman14377@gmail.com>
2023-02-17 19:05:44 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,8 @@
 *main - Entry point
 *Return: Always 0 (success)
 */
-
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
